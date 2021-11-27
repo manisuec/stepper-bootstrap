eea1e804757da3aa02a4c5e68e0f4e0b68189b92
Manish Prasad <manishvaishu25@gmail.com>
2021-11-27 12:40:08 +0530

    refactored more

---
+++ src/Stepper/Step.js
@@ -0,0 +1,18 @@
+import { forwardRef } from 'react';
+import { Card } from 'react-bootstrap';
+
+const Step = forwardRef(function Step(props, ref){
+  const {
+    children,
+    className,
+    ...restProps
+  } = props;
+
+  return (
+    <Card.Body ref={ref} {...restProps} className={className}>
+      {children}
+    </Card.Body>
+  )
+});
+
+export default Step;
